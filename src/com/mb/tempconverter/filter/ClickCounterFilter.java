b3dd91435e124f6cd447d9bb592813e579bda1d3
ubuntu-orca <webmasterbengrich@gmail.com>
2017-02-20 18:04:08 +0000

    counter filter

--- src/com/mb/tempconverter/filter/ClickCounterFilter.java
+++ src/com/mb/tempconverter/filter/ClickCounterFilter.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+
+import com.sun.net.httpserver.Filter.Chain;
 
 public class ClickCounterFilter implements Filter {
 
@@ -18,16 +21,26 @@ public class ClickCounterFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		System.out.println("ClickCounterFilter - doFilter");
+		
+		ServletContext sc = req.getServletContext();
+		int counter = (int) sc.getAttribute("counter");
+		counter++;
+		sc.setAttribute("counter", counter);
+		System.out.println("ClickCounterFilter - "+sc.getAttribute("counter"));
+		chain.doFilter(req, res);
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
+	public void init(FilterConfig filterConfig) throws ServletException {
 		// TODO Auto-generated method stub
 		System.out.println("ClickCounterFilter - init");
+		int counter = 0;
+		ServletContext sc = filterConfig.getServletContext();
+		sc.setAttribute("counter", counter);
 	}
 
 }
